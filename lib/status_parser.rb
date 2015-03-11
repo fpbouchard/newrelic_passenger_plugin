beb793ce507b7149923f3c0d14fd6abadec568ea
François-Pierre Bouchard <fpbouchard@gmail.com>
2015-03-10 23:19:09 -0400

    Index metrics per worker index, change metric names and units

--- lib/status_parser.rb
+++ lib/status_parser.rb
@@ -21,67 +21,52 @@ class Status < OpenStruct
   private
 
   def parse_cpu(doc)
-    cpu_total = 0
-    cpu_per_process = {}
-    doc.xpath('//process').each do |process_node|
-      pid = process_node.xpath('pid').text.to_i
-      this_cpu = process_node.xpath('cpu').text.to_f
-      cpu_total += this_cpu
-      cpu_per_process[pid] = this_cpu
-    end
-    self.cpu_total = cpu_total
-    self.cpu_per_process = cpu_per_process
+    total = 0
+    self.cpu_per_worker = Hash[doc.xpath('//process/cpu').each_with_index.collect do |node, idx|
+      this_cpu = node.text.to_f
+      total += this_cpu
+      [idx, this_cpu]
+    end]
+    self.cpu_total = total
   end
 
   def parse_memory(doc)
-    mem_total = 0
-    processes = {}
-    doc.xpath('//process').each do |process_node|
-      pid = process_node.xpath('pid').text.to_i
-      this_mem = process_node.xpath('./real_memory').text.to_i / 1024
-      mem_total += this_mem
-      processes[pid] = this_mem
-    end
-    self.memory_per_process = processes
-    self.memory_total = mem_total
+    total = 0
+    self.memory_per_worker = Hash[doc.xpath('//process/real_memory').each_with_index.collect do |node, idx|
+      this_mem = node.text.to_i
+      total += this_mem
+      [idx, this_mem]
+    end]
+    self.memory_total = total
   end
 
   def parse_last_used(doc)
-    processes = {}
-    doc.xpath('//process').each do |process_node|
-      pid = process_node.xpath('pid').text.to_i
-      unix_stamp = (process_node.xpath('./last_used').text.to_i / 1000000)
+    self.last_used_time_per_worker = Hash[doc.xpath('//process/last_used').each_with_index.collect do |node, idx|
+      unix_stamp = (node.text.to_i / 1000000)
       elapsed = Time.now.to_i - unix_stamp
-      processes[pid] = elapsed
-    end
-    self.last_used_time_per_process = processes
+      [idx, elapsed]
+    end]
   end
 
   def parse_uptime(doc)
-    processes = {}
-    doc.xpath('//process').each do |process_node|
-      pid = process_node.xpath('pid').text.to_i
-      unix_stamp = (process_node.xpath('./spawn_end_time').text.to_i / 1000000)
+    self.uptime_per_worker = Hash[doc.xpath('//process/spawn_end_time').each_with_index.collect do |node, idx|
+      unix_stamp = (node.text.to_i / 1000000)
       elapsed = Time.now.to_i - unix_stamp
-      processes[pid] = elapsed
-    end
-    self.uptime_per_process = processes
+      [idx, elapsed]
+    end]
   end
 
   def parse_requests(doc)
     self.request_queue_size = doc.xpath('//info/get_wait_list_size').text.to_i
 
-    requests_total = 0
-    processes = {}
-    doc.xpath('//process').each do |process_node|
-      pid = process_node.xpath('pid').text.to_i
-      requests = process_node.xpath('./processed').text.to_i
-      requests_total += requests
-      processes[pid] = requests
-    end
+    total = 0
+    self.requests_per_worker = Hash[doc.xpath('//process/processed').each_with_index.collect do |node, idx|
+      requests = node.text.to_i
+      total += requests
+      [idx, requests]
+    end]
 
-    self.requests_total = requests_total
-    self.requests_per_process = processes
+    self.requests_total = total
   end
 
 end
--- spec/passenger_status_spec.rb
+++ spec/passenger_status_spec.rb
@@ -19,23 +19,25 @@ describe Status do
 
       it "collects CPU information" do
         expect(status.cpu_total).to eq 4.5
-        expect(status.cpu_per_process).to eq({23882 => 1.0, 23892 => 1.5, 23902 => 2.0, 23912 => 0.0, 23922 => 0.0, 23932 => 0.0})
+        expect(status.cpu_per_worker).to eq({0 => 1.0, 1 => 1.5, 2 => 2.0, 3 => 0.0, 4 => 0.0, 5 => 0.0})
       end
 
       it "collects memory information" do
         expect(status.memory_total).to eq 262
-        expect(status.memory_per_process).to eq({23882=>54, 23892=>31, 23902=>36, 23912=>30, 23922=>55, 23932=>56})
+        expect(status.memory_per_worker).to eq({0=>54, 1=>31, 2=>36, 3=>30, 4=>55, 5=>56})
       end
 
       it "collects usage timings" do
         Timecop.freeze(Date.new(2014, 3, 15)) do
-          expect(status.last_used_time_per_process).to eq({23882=>379736, 23892=>386339, 23902=>382567, 23912=>386338, 23922=>379151, 23932=>377501})
-          expect(status.uptime_per_process).to eq({23882=>386339, 23892=>386339, 23902=>386339, 23912=>386338, 23922=>386338, 23932=>386338})
+          expect(status.last_used_time_per_worker).to eq({0=>379736, 1=>386339, 2=>382567, 3=>386338, 4=>379151, 5=>377501})
+          expect(status.uptime_per_worker).to eq({0=>386339, 1=>386339, 2=>386339, 3=>386338, 4=>386338, 5=>386338})
         end
       end
 
       it "collects request information" do
         expect(status.request_queue_size).to eq 3
+        expect(status.requests_total).to eq 3257
+        expect(status.requests_per_worker).to eq({0=>1808, 1=>0, 2=>1, 3=>0, 4=>327, 5=>1121})
       end
 
     end
